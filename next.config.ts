897573b2fda5796531af18e67b9785a710330ace
Thijs <hi@thijs.gg>
2024-12-28 19:02:34 -0800

    bump versions

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:slug*',
-        destination: 'http://localhost:9000/api/:slug*',
-      },
-    ];
-  },
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
